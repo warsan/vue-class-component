e838ee62828aa1c7f0008be515536edeabbcb8e9
Сандро <warsan00@mail.ru>
2020-10-01 08:10:42 +0500

    Правки

--- example/src/components/World.tsx
+++ example/src/components/World.tsx
@@ -4,6 +4,6 @@ import Component from '../../../lib/index'
 @Component
 export default class World extends Vue {
   render (h: CreateElement) {
-    return <p>This is rendered via TSX</p>
+    return <p>Это отображается через TSX</p>
   }
 }
